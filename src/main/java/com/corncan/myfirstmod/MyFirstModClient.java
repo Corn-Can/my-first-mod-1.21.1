a6af92d9f57b0f6dc0b65cc425c7f4b6de4275a4
Corn-Can <andy1021128@gmail.com>
2026-01-08 19:56:43 +0800

    Initial commit

---
+++ src/main/java/com/corncan/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package com.corncan.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
